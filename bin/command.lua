cf177d6ebfb32a9963d40b046bfc4eb02436ec11
Masatoshi Teruya <mah0x211@gmail.com>
2018-05-11 14:17:50 +0900

    Change command.lua to print execution parameters

--- bin/command.lua
+++ bin/command.lua
@@ -9,13 +9,35 @@
 --]]
 require('signal').blockAll()
 local Tempest = require('tempest')
+local Script = require('tempest.script')
 local getopts = require('tempest.getopts')
+local strformat = string.format
 local opts = getopts(...)
 
+
+-- print execution parameters
+print(strformat([[
+tempest run with following options;
+-----------------------------------
+    address: %q
+     worker: %s
+     client: %s
+   duration: %s
+   rcvtimeo: %s
+   sndtimeo: %s
+     script: %q
+   loglevel: %s
+-----------------------------------]],
+    opts[-1].addr, opts[-1].worker, opts[-1].client, opts[-1].duration,
+    opts[-1].rcvtimeo, opts[-1].sndtimeo, opts[-1].script or 'NOT SPECIFIED',
+    opts[-1].loglevel
+))
+
+
+-- set loglevel
 if opts.loglevel then
     require('tempest.logger').setlevel( opts.loglevel )
 end
-log('start')
 
 local ok, err = require('act').run(function()
     local t = Tempest.new( opts.worker )
