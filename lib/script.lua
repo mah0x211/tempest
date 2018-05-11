859124f44665dc0f66635da29aa0084071f96ee1
Masatoshi Teruya <mah0x211@gmail.com>
2018-05-11 11:27:27 +0900

    Change returns of script.compileFile to the chunk

--- lib/script.lua
+++ lib/script.lua
@@ -14,6 +14,7 @@ local tofile = require('path').tofile
 local getcwd = require('process').getcwd
 local strformat = string.format
 local strsub = string.sub
+local strdump = string.dump
 -- constants
 local ENV = require('tempest.env')
 
@@ -33,10 +34,11 @@ end
 
 --- compileFile
 -- @param pathname
--- @return fn
+-- @return chunk
 -- @return err
 local function compileFile( pathname )
     local fullpath, err = realpath( pathname )
+    local fn
 
     if err then
         return nil, err
@@ -44,7 +46,12 @@ local function compileFile( pathname )
         return nil, strformat( '%q not found', pathname )
     end
 
-    return loadchunk.file( fullpath, ENV )
+    fn, err = loadchunk.file( fullpath )
+    if err then
+        return nil, err
+    end
+
+    return strdump( fn )
 end
 
 
